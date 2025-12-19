67792ab8d6c1ade82fa84117d82dba24559150d8
codewithrajan1-ship-it <codewithrajan1@gmail.com>
2025-12-19 12:27:34 +0000

    CONTAINER CONFIG DONE

---
+++ BASE_1 JAVASCRIPT/01_VARIABLE.js
@@ -0,0 +1,17 @@
+const accountID = 1234561
+let accountEmail = "rajansahu@google.com"
+var accountPassword = "12345" 
+ accountcity = "Jaipur" /* :- This is not us a variable */
+let accountstate;
+// accountID = 2 // not allowed 
+/*  VAR IS NOT BE USE IN JAVASCRIPT */ 
+
+accountEmail = "sahu@rajan.com"
+accountPassword = "346742"
+accountcity = "valsad"
+
+console.log("accountID");
+
+console.table([accountEmail,accountID,accountPassword,accountcity])
+
+
